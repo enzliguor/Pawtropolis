c39f8d9ee50b7a5dae53bd7c1992d123c23cf118
Vincenzo Liguori <enzo.liguori.93@hotmail.it>
2023-03-02 15:35:24 +0100

    feat(Application): marks this as a SpringBoot Application

--- app/src/main/java/pawtropolis/complex/Application.java
+++ app/src/main/java/pawtropolis/complex/Application.java
@@ -1,11 +1,17 @@
 package pawtropolis.complex;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import pawtropolis.complex.game.GameController;
 
+@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		GameController.getInstance().runGame();
+		ConfigurableApplicationContext conf = SpringApplication.run(Application.class, args);
+		GameController game =  conf.getBean(GameController.class);
+		game.runGame();
 	}
 
 }
