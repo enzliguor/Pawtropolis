5c2c5d2dd1a559cb036139e605f7e329aafa1481
Vincenzo Liguori <enzo.liguori.93@hotmail.it>
2023-03-23 18:11:25 +0100

    feature(CommandTrigger): adds an enum class that links each commandTrigger to the corresponding command class

---
+++ app/src/main/java/pawtropolis/complex/game/command/CommandTrigger.java
@@ -0,0 +1,37 @@
+package pawtropolis.complex.game.command;
+
+import lombok.Getter;
+import pawtropolis.complex.game.command.domain.*;
+
+import java.util.Arrays;
+
+public enum CommandTrigger {
+    BAG("bag", BagCommand.class),
+    DROP("drop", DropCommand.class),
+    EXIT("exit", ExitCommand.class),
+    GET("get", GetCommand.class),
+    GO("go", GoCommand.class),
+    HELP("help", HelpCommand.class),
+    LOOK("look", LookCommand.class),
+    WRONG_COMMAND("wrong command", WrongCommand.class);
+
+    @Getter
+    private final String trigger;
+
+    @Getter
+    private final Class<? extends Command> commandClass;
+
+    CommandTrigger(String trigger, Class<? extends Command> commandClass){
+        this.trigger = trigger;
+        this.commandClass = commandClass;
+    }
+
+    public static CommandTrigger of(String trigger){
+        return Arrays.stream(CommandTrigger.values())
+                .filter(commandTrigger -> commandTrigger.getTrigger().equalsIgnoreCase(trigger))
+                .findFirst()
+                .orElse(WRONG_COMMAND);
+    }
+
+
+}
