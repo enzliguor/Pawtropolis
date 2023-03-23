0111c2b1d55ce8692c205d8511d3d682df09c05d
Vincenzo Liguori <enzo.liguori.93@hotmail.it>
2023-03-23 18:26:13 +0100

    refactor(GameController): adds the Player and Room fields to GameController

--- app/src/main/java/pawtropolis/complex/game/GameController.java
+++ app/src/main/java/pawtropolis/complex/game/GameController.java
@@ -2,26 +2,40 @@ package pawtropolis.complex.game;
 
 
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 import pawtropolis.complex.game.command.CommandManager;
 import pawtropolis.complex.console.InputController;
+import pawtropolis.complex.game.domain.Player;
+import pawtropolis.complex.game.map.domain.Room;
+import pawtropolis.complex.game.map.mapinitializer.MapInitializer;
 
 @Slf4j
 @ToString
 @Component
 public class GameController implements ApplicationRunner {
     @Getter
+    private Player player;
+
+    @Getter
+    @Setter
+    private Room currentRoom;
+
+    @Getter
     private boolean gameRunning;
 
     private final CommandManager commandManager;
 
     @Autowired
-    private GameController(CommandManager commandManager) {
+    private GameController(MapInitializer mapInitializer, CommandManager commandManager) {
+        this.gameRunning = true;
+        this.currentRoom = mapInitializer.populateMap();
         this.commandManager = commandManager;
         this.gameRunning = false;
     }
@@ -31,7 +45,7 @@ public class GameController implements ApplicationRunner {
         while (gameRunning) {
             log.info("Where are you going to go?>");
 
-            commandManager.execute(InputController.readString());
+            commandManager.executeCommand(InputController.readString());
         }
     }
     public void endGame() {
