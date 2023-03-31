f2dfaba90befc1c22c5a240fb117e771981dffd9
Vincenzo Liguori <enzo.liguori.93@hotmail.it>
2023-03-31 16:50:52 +0200

    refactor(Room): changes the way Room handles Items and adapts its add and remove methods. It now always references the same Item instance, but keeps track of how many Items of the same type it contains.

--- app/src/main/java/pawtropolis/complex/game/map/domain/Room.java
+++ app/src/main/java/pawtropolis/complex/game/map/domain/Room.java
@@ -18,98 +18,120 @@ import java.util.stream.Collectors;
 @Entity
 @Table(name = "room")
 public class Room {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	@Getter
-	@Setter
-	@Column(name = "name")
-	private String name;
-	@OneToMany
-	@JoinTable(name = "items_in_room",
-			joinColumns = {@JoinColumn(name = "id_item", referencedColumnName = "id")},
-			inverseJoinColumns = {@JoinColumn(name = "id_room", referencedColumnName = "id")})
-	private final List<Item> items = new ArrayList<>();
-	@OneToMany
-	@JoinColumn(name = "id_room", referencedColumnName = "id")
-	private final List<Animal> animals = new ArrayList<>();
-	@ManyToMany
-	@JoinTable(name= "linked_rooms",
-			joinColumns = {
-					@JoinColumn(name = "id_room", referencedColumnName = "id")},
-			inverseJoinColumns = {
-					@JoinColumn(name = "id_adjacent_room", referencedColumnName = "id")
-			})
-	@MapKeyEnumerated(value = EnumType.STRING)
-	private final Map<CardinalPoint, Room> adjacentRooms = new EnumMap<>(CardinalPoint.class);
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Getter
+    @Setter
+    @Column(name = "name")
+    private String name;
 
-	public Room(String name) {
-		this.name = name;
-	}
+    @ElementCollection
+    @CollectionTable(name = "items_in_room",
+            joinColumns = {@JoinColumn(name = "id_room", referencedColumnName = "id")})
+    @MapKeyJoinColumn(name = "id_item", referencedColumnName = "id")
+    @Column(name = "quantity")
+    private Map<Item, Integer> items = new HashMap<>();
+    @OneToMany
+    @JoinColumn(name = "id_room", referencedColumnName = "id")
+    private final List<Animal> animals = new ArrayList<>();
+    @ManyToMany
+    @JoinTable(name = "linked_rooms",
+            joinColumns = {
+                    @JoinColumn(name = "id_room", referencedColumnName = "id")},
+            inverseJoinColumns = {
+                    @JoinColumn(name = "id_adjacent_room", referencedColumnName = "id")
+            })
+    @MapKeyEnumerated(value = EnumType.STRING)
+    @MapKeyColumn(name = "cardinal_point")
+    private final Map<CardinalPoint, Room> adjacentRooms = new EnumMap<>(CardinalPoint.class);
 
-	public Room getAdjacentRoom(CardinalPoint cardinalPoint) {
-		return this.adjacentRooms.get(cardinalPoint);
-	}
+    public Room(String name) {
+        this.name = name;
+    }
 
-	public List<Item> getItems(){
-		return List.copyOf(this.items);
-	}
+    public Room getAdjacentRoom(CardinalPoint cardinalPoint) {
+        return this.adjacentRooms.get(cardinalPoint);
+    }
 
-	public void removeItem(Item item) {
-		this.items.remove(item);
-	}
+    public Map<Item, Integer> getItems() {
+        return Map.copyOf(this.items);
+    }
 
-	public void addItem(Item item) {
-		this.items.add(item);
-	}
+    public void removeItem(Item item) {
+        removeItems(item, 1);
+    }
 
-	public void addAllItems(List<Item> items) {
-		items.forEach(this::addItem);
-	}
+    public void removeItems(Item item, Integer integer) {
+        if (item != null && this.items.containsKey(item) && getItemQuantity(item) >= integer) {
+            addItem(item, getItemQuantity(item) - integer);
+            if (getItemQuantity(item) == 0) {
+                this.items.remove(item);
+            }
+        }
+    }
 
-	public void addAnimal(Animal animal) {
-		this.animals.add(animal);
-	}
+    public void addItem(Item item) {
+        addItem(item, 1);
+    }
 
-	public void addAllAnimals(List<Animal> animals) {
-		animals.forEach(this::addAnimal);
-	}
+    public void addItem(Item item, Integer integer) {
+        if (item != null) {
+            this.items.put(item, this.items.getOrDefault(item, 0) + integer);
+        }
+    }
 
-	public void removeAnimal(Animal animal) {
-		animals.remove(animal);
-	}
+    public void addAllItems(List<Item> items) {
+        items.forEach(this::addItem);
+    }
 
-	public void linkRoom(CardinalPoint cardinalPoint, Room room){
-		this.adjacentRooms.put(cardinalPoint, room);
-		CardinalPoint opposite = cardinalPoint.getOpposite();
-		Room oppositeRoom = room.getAdjacentRoom(opposite);
-		if(oppositeRoom != this){
-			room.linkRoom(opposite, this);
-		}
-	}
+    public void addAnimal(Animal animal) {
+        this.animals.add(animal);
+    }
 
-	public  List<Animal> getAnimals(){
-		return List.copyOf(this.animals);
-	}
+    public void addAllAnimals(List<Animal> animals) {
+        animals.forEach(this::addAnimal);
+    }
 
-	public Item findItemByName(String itemName) {
-		return this.items.stream()
-				.filter(i -> i.getName().equals(itemName))
-				.findFirst()
-				.orElse(null);
-	}
+    public void removeAnimal(Animal animal) {
+        animals.remove(animal);
+    }
 
-	public List<String> getItemsName() {
-		return this.items.stream()
-				.map(Item::getName)
-				.toList();
-	}
+    public void linkRoom(CardinalPoint cardinalPoint, Room room) {
+        this.adjacentRooms.put(cardinalPoint, room);
+        CardinalPoint opposite = cardinalPoint.getOpposite();
+        Room oppositeRoom = room.getAdjacentRoom(opposite);
+        if (oppositeRoom != this) {
+            room.linkRoom(opposite, this);
+        }
+    }
 
-	public Map<Class<? extends Animal>, List<String>> getAnimalsName() {
-		return this.animals.stream()
-				.collect(Collectors.groupingBy(
-						Animal::getClass,
-						Collectors.mapping(Animal::getName, Collectors.toList())
-						));
-	}
+    public List<Animal> getAnimals() {
+        return List.copyOf(this.animals);
+    }
+
+    public Item findItemByName(String itemName) {
+        return this.items.keySet().stream()
+                .filter(i -> i.getName().equals(itemName))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public Integer getItemQuantity(Item item) {
+        return this.items.get(item);
+    }
+
+    public List<String> getItemsName() {
+        return this.items.keySet().stream()
+                .map(Item::getName)
+                .toList();
+    }
+
+    public Map<Class<? extends Animal>, List<String>> getAnimalsName() {
+        return this.animals.stream()
+                .collect(Collectors.groupingBy(
+                        Animal::getClass,
+                        Collectors.mapping(Animal::getName, Collectors.toList())
+                ));
+    }
 }
