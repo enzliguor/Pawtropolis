5e2baf86164c05aa6a2a138452544a1d6e1b3899
Vincenzo Liguori <enzo.liguori.93@hotmail.it>
2023-04-19 14:11:26 +0200

    refactor(LionMarshaller): delegates to the AnimalBuilderDirector class the construction of the Lion and LionBO instances

--- app/src/main/java/pawtropolis/complex/marshaller/LionMarshaller.java
+++ app/src/main/java/pawtropolis/complex/marshaller/LionMarshaller.java
@@ -3,6 +3,7 @@ package pawtropolis.complex.marshaller;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
+import pawtropolis.complex.game.animals.AnimalBuilderDirector;
 import pawtropolis.complex.game.animals.domain.LionBO;
 import pawtropolis.complex.persistence.entity.Lion;
 
@@ -10,31 +11,13 @@ import pawtropolis.complex.persistence.entity.Lion;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class LionMarshaller implements Marshaller<Lion, LionBO> {
     @Override
-    public Lion marshall(LionBO animalBO) {
-        return Lion.builder()
-                .id(animalBO.getId())
-                .favoriteFood(animalBO.getFavoriteFood())
-                .age(animalBO.getAge())
-                .weight(animalBO.getWeight())
-                .joinDate(animalBO.getJoinDate())
-                .height(animalBO.getHeight())
-                .name(animalBO.getName())
-                .tailLength(animalBO.getTailLength())
-                .build();
+    public Lion marshall(LionBO lionBO) {
+        return AnimalBuilderDirector.buildLion(lionBO);
     }
 
     @Override
-    public LionBO unmarshall(Lion animal) {
-        return LionBO.builder()
-                .id(animal.getId())
-                .favoriteFood(animal.getFavoriteFood())
-                .age(animal.getAge())
-                .weight(animal.getWeight())
-                .joinDate(animal.getJoinDate())
-                .height(animal.getHeight())
-                .name(animal.getName())
-                .tailLength(animal.getTailLength())
-                .build();
+    public LionBO unmarshall(Lion lion) {
+        return AnimalBuilderDirector.buildLionBO(lion);
     }
 
     @Override
