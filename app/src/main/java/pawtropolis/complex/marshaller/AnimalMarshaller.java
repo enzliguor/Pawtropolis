5b5128ba34fa2820d37e1c529b2e72e6ccc53c08
Vincenzo Liguori <enzo.liguori.93@hotmail.it>
2023-04-19 09:55:22 +0200

    feature(AnimalMarshaller): adds a Marshaller that handles all concrete Animal Marshaller

---
+++ app/src/main/java/pawtropolis/complex/marshaller/AnimalMarshaller.java
@@ -0,0 +1,70 @@
+package pawtropolis.complex.marshaller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+import pawtropolis.complex.game.animals.domain.AnimalBO;
+import pawtropolis.complex.persistence.entity.Animal;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class AnimalMarshaller implements Marshaller<Animal, AnimalBO> {
+
+    private final Map<Class<?>, Marshaller<Object, Object>> animalmarshallers;
+
+    @Autowired
+    private AnimalMarshaller(ApplicationContext applicationContext) {
+        this.animalmarshallers = new HashMap<>();
+        applicationContext.getBeansOfType(Marshaller.class).values().stream()
+                .filter(marshaller -> Animal.class.isAssignableFrom(marshaller.getEntityClass()) ||
+                        AnimalBO.class.isAssignableFrom(marshaller.getBoClass()))
+                .forEach(marshaller -> {
+                    this.animalmarshallers.put(marshaller.getEntityClass(), marshaller);
+                    this.animalmarshallers.put(marshaller.getBoClass(), marshaller);
+                });
+    }
+
+    @Override
+    public Animal marshall(AnimalBO animalBO) {
+        if (animalBO == null) {
+            return null;
+        }
+        Marshaller<Object, Object> marshaller = this.animalmarshallers.get(animalBO.getClass());
+        return (marshaller != null)? (Animal) marshaller.marshall(marshaller.getBoClass().cast(animalBO)) : null;
+    }
+
+    @Override
+    public AnimalBO unmarshall(Animal animal) {
+        if (animal == null) {
+            return null;
+        }
+        Marshaller<Object, Object> marshaller = this.animalmarshallers.get(animal.getClass());
+        return (AnimalBO) marshaller.unmarshall(marshaller.getEntityClass().cast(animal));
+
+    }
+
+    public List<Animal> marshall(List<AnimalBO> animalBoList) {
+        return animalBoList.stream()
+                .map(this::marshall)
+                .toList();
+    }
+
+    public List<AnimalBO> unmarshall(List<Animal> animals) {
+        return animals.stream()
+                .map(this::unmarshall)
+                .toList();
+    }
+
+    @Override
+    public Class<Animal> getEntityClass() {
+        return Animal.class;
+    }
+
+    @Override
+    public Class<AnimalBO> getBoClass() {
+        return AnimalBO.class;
+    }
+}
