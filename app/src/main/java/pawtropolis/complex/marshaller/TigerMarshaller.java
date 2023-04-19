1e0e25a4d5cbb40d5b6851a88ce73996e20519c3
Vincenzo Liguori <enzo.liguori.93@hotmail.it>
2023-04-19 14:11:59 +0200

    refactor(TigerMarshaller): delegates to the AnimalBuilderDirector class the construction of the Tiger and TigerBO instances

--- app/src/main/java/pawtropolis/complex/marshaller/TigerMarshaller.java
+++ app/src/main/java/pawtropolis/complex/marshaller/TigerMarshaller.java
@@ -3,6 +3,7 @@ package pawtropolis.complex.marshaller;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
+import pawtropolis.complex.game.animals.AnimalBuilderDirector;
 import pawtropolis.complex.game.animals.domain.TigerBO;
 import pawtropolis.complex.persistence.entity.Tiger;
 
@@ -10,31 +11,13 @@ import pawtropolis.complex.persistence.entity.Tiger;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class TigerMarshaller implements Marshaller<Tiger, TigerBO> {
     @Override
-    public Tiger marshall(TigerBO animalBO) {
-        return Tiger.builder()
-                .id(animalBO.getId())
-                .favoriteFood(animalBO.getFavoriteFood())
-                .age(animalBO.getAge())
-                .weight(animalBO.getWeight())
-                .joinDate(animalBO.getJoinDate())
-                .height(animalBO.getHeight())
-                .name(animalBO.getName())
-                .tailLength(animalBO.getTailLength())
-                .build();
+    public Tiger marshall(TigerBO tigerBO) {
+        return AnimalBuilderDirector.buildTiger(tigerBO);
     }
 
     @Override
-    public TigerBO unmarshall(Tiger animal) {
-        return TigerBO.builder()
-                .id(animal.getId())
-                .favoriteFood(animal.getFavoriteFood())
-                .age(animal.getAge())
-                .weight(animal.getWeight())
-                .joinDate(animal.getJoinDate())
-                .height(animal.getHeight())
-                .name(animal.getName())
-                .tailLength(animal.getTailLength())
-                .build();
+    public TigerBO unmarshall(Tiger tiger) {
+        return AnimalBuilderDirector.buildTigerBO(tiger);
     }
 
     @Override
