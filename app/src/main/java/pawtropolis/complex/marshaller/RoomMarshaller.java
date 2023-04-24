c4570f492aa8d4a63c21d1396bdcdb477fc1943b
enzo.liguori <enzo.liguori.93@hotmail.it>
2023-04-24 17:53:32 +0200

    feature(RoomMarshaller): implements the behavior of the RoomMarshaller which now given a room is able to convert it along with all the rooms connected to it

--- app/src/main/java/pawtropolis/complex/marshaller/RoomMarshaller.java
+++ app/src/main/java/pawtropolis/complex/marshaller/RoomMarshaller.java
@@ -3,8 +3,11 @@ package pawtropolis.complex.marshaller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import pawtropolis.complex.game.map.domain.RoomBO;
+import pawtropolis.complex.game.map.util.CardinalPoint;
 import pawtropolis.complex.persistence.entity.Room;
 
+import java.util.EnumMap;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -23,6 +26,32 @@ public class RoomMarshaller implements Marshaller<Room, RoomBO> {
 
     @Override
     public Room marshall(RoomBO roomBO) {
+        return marshallAllLinkedRoomBO(roomBO);
+    }
+
+    private Room marshallAllLinkedRoomBO(RoomBO roomBO) {
+        Map<RoomBO, Room> convertedRooms = marshallAllLinkedRoomBO(roomBO, new HashMap<>());
+        return convertedRooms.get(roomBO);
+    }
+
+    private Map<RoomBO, Room> marshallAllLinkedRoomBO(RoomBO roomBO, Map<RoomBO, Room> convertedRooms) {
+        if (!convertedRooms.containsKey(roomBO)) {
+            Room room = marshallSingleRoomBO(roomBO);
+            convertedRooms.put(roomBO, room);
+            roomBO.getAdjacentRooms().forEach((cardinal, adjacentRoomBO) -> {
+                marshallAllLinkedRoomBO(adjacentRoomBO, convertedRooms);
+                Room adjacentRoom = convertedRooms.get(adjacentRoomBO);
+                linkRooms(room, cardinal, adjacentRoom);
+            });
+        }
+        return convertedRooms;
+    }
+
+    private void linkRooms(Room room1, CardinalPoint cardinalPoint, Room room2) {
+        room1.getAdjacentRooms().put(cardinalPoint, room2);
+    }
+
+    private Room marshallSingleRoomBO(RoomBO roomBO) {
         return Room.builder()
                 .id(roomBO.getId())
                 .name(roomBO.getName())
@@ -31,14 +60,36 @@ public class RoomMarshaller implements Marshaller<Room, RoomBO> {
                                 entry -> this.itemConverter.marshall(entry.getKey()),
                                 Map.Entry::getValue
                         )))
+                .adjacentRooms(new EnumMap<>(CardinalPoint.class))
                 .animals(this.animalMarshaller.marshall(roomBO.getAnimals()))
-                //TODO Implement behavior regarding adjacent rooms
-//                .adjacentRooms()
                 .build();
     }
 
     @Override
     public RoomBO unmarshall(Room room) {
+        return unmarshallAllLinkedRooms(room);
+    }
+
+    private RoomBO unmarshallAllLinkedRooms(Room room) {
+        Map<Room, RoomBO> convertedRooms = unmarshallAllLinkedRooms(room, new HashMap<>());
+        return convertedRooms.get(room);
+    }
+
+    private Map<Room, RoomBO> unmarshallAllLinkedRooms(Room room, Map<Room, RoomBO> convertedRooms) {
+        if (!convertedRooms.containsKey(room)) {
+            RoomBO roomBO = unmarshallSingleRoom(room);
+            convertedRooms.put(room, roomBO);
+            room.getAdjacentRooms().forEach(((cardinal, adjacentRoom) -> {
+                unmarshallAllLinkedRooms(adjacentRoom, convertedRooms);
+                RoomBO adjacentRoomBO = convertedRooms.get(adjacentRoom);
+                roomBO.linkRoom(cardinal, adjacentRoomBO);
+            }));
+        }
+        return convertedRooms;
+    }
+
+
+    private RoomBO unmarshallSingleRoom(Room room) {
         return RoomBO.builder()
                 .id(room.getId())
                 .name(room.getName())
@@ -48,8 +99,7 @@ public class RoomMarshaller implements Marshaller<Room, RoomBO> {
                                 Map.Entry::getValue
                         )))
                 .animals(this.animalMarshaller.unmarshall(room.getAnimals()))
-                //TODO Implement behavior regarding adjacent rooms
-//                .adjacentRooms()
+                .adjacentRooms(new EnumMap<>(CardinalPoint.class))
                 .build();
     }
 
