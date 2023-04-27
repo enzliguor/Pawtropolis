3da5bf31710787e8c33b692e00aecfbf414d471a
enzo.liguori <enzo.liguori.93@hotmail.it>
2023-04-27 23:57:45 +0200

    feature(PlayerService): adds service for Player entity

---
+++ app/src/main/java/pawtropolis/complex/persistence/service/PlayerService.java
@@ -0,0 +1,16 @@
+package pawtropolis.complex.persistence.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+import pawtropolis.complex.game.domain.PlayerBO;
+import pawtropolis.complex.persistence.entity.Player;
+import pawtropolis.complex.utils.MarshallerManager;
+@Service
+public class PlayerService extends AbstractService<Player, Long, PlayerBO>{
+
+    @Autowired
+    private PlayerService(JpaRepository<Player, Long> dao, MarshallerManager marshallerManager) {
+        super(dao, marshallerManager);
+    }
+}
