c193cb7631ca5076bc7be336a682fbf64cac36f5
enzo.liguori <enzo.liguori.93@hotmail.it>
2023-05-01 22:38:06 +0200

    feature(Marshaller): adds two default methods that handle List of elements

--- app/src/main/java/pawtropolis/persistence/marshaller/Marshaller.java
+++ app/src/main/java/pawtropolis/persistence/marshaller/Marshaller.java
@@ -3,6 +3,8 @@ package pawtropolis.persistence.marshaller;
 import pawtropolis.game.domain.BusinessObject;
 import pawtropolis.persistence.entity.EntityDB;
 
+import java.util.List;
+
 public interface Marshaller<E extends EntityDB, B extends BusinessObject> {
 
     E marshall(B businessObject);
@@ -12,4 +14,12 @@ public interface Marshaller<E extends EntityDB, B extends BusinessObject> {
     Class<E> getEntityClass();
 
     Class<B> getBoClass();
+
+    default List<E> marshall(List<B> businessObjects) {
+        return businessObjects.stream().map(this::marshall).toList();
+    }
+
+    default List<B> unmarshall(List<E> entities) {
+        return entities.stream().map(this::unmarshall).toList();
+    }
 }
