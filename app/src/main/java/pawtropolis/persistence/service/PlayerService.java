ddb672d1a92a785fba46c68394f1d877a01c586a
enzo.liguori <enzo.liguori.93@hotmail.it>
2023-05-04 15:20:14 +0200

    feature(PlayerService): adds the 'findByName' method

--- app/src/main/java/pawtropolis/persistence/service/PlayerService.java
+++ app/src/main/java/pawtropolis/persistence/service/PlayerService.java
@@ -3,10 +3,13 @@ package pawtropolis.persistence.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import pawtropolis.game.domain.PlayerBO;
 import pawtropolis.persistence.dao.PlayerDAO;
 import pawtropolis.persistence.entity.Player;
 import pawtropolis.persistence.marshaller.Marshaller;
+
+import java.util.Optional;
 
 @Service
 public class PlayerService extends AbstractService<Player, Long, PlayerBO>{
@@ -17,5 +20,14 @@ public class PlayerService extends AbstractService<Player, Long, PlayerBO>{
 
     public boolean existsByName(String name){
         return ((PlayerDAO)dao).existsByName(name);
+    }
+
+    @Transactional
+    public PlayerBO findByName(String name){
+        if(name == null){
+            return null;
+        }
+        Optional<Player> playerOpt = ((PlayerDAO)dao).findByName(name);
+        return this.marshaller.unmarshall(playerOpt.orElse(null));
     }
 }
