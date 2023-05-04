a5d8bfa216dbf5e5ba3e7f122b5bf09795ecccd7
enzo.liguori <enzo.liguori.93@hotmail.it>
2023-05-04 15:03:09 +0200

    feature(PlayerDAO): adds the method 'findByName'

--- app/src/main/java/pawtropolis/persistence/dao/PlayerDAO.java
+++ app/src/main/java/pawtropolis/persistence/dao/PlayerDAO.java
@@ -4,9 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pawtropolis.persistence.entity.Player;
 
+import java.util.Optional;
+
 @Repository
 public interface PlayerDAO extends JpaRepository<Player, Long> {
 
     boolean existsByName(String name);
+    Optional<Player> findByName(String name);
 
 }
