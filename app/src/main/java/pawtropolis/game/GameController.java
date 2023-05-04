483ad7f5ca3fd41782a7c4e1a2fe11bcc526f7d1
enzo.liguori <enzo.liguori.93@hotmail.it>
2023-05-04 15:28:12 +0200

    refactor(GameController):  from the GameController the responsibility for maintaining the status of the game

--- app/src/main/java/pawtropolis/game/GameController.java
+++ app/src/main/java/pawtropolis/game/GameController.java
@@ -2,7 +2,6 @@ package pawtropolis.game;
 
 
 import lombok.Getter;
-import lombok.Setter;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,41 +9,25 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 import pawtropolis.game.command.CommandManager;
-import pawtropolis.console.InputController;
-import pawtropolis.game.domain.PlayerBO;
-import pawtropolis.game.domain.RoomBO;
-import pawtropolis.game.map.initializer.MapInitializer;
 
 @Slf4j
 @ToString
 @Component
 public class GameController implements ApplicationRunner {
     @Getter
-    private PlayerBO player;
-    @Getter
-    @Setter
-    private RoomBO currentRoom;
-    @Getter
     private boolean gameRunning;
     private final CommandManager commandManager;
 
     @Autowired
-    private GameController(MapInitializer mapInitializer, CommandManager commandManager) {
+    private GameController(CommandManager commandManager) {
         this.gameRunning = false;
-        this.currentRoom = mapInitializer.populateMap();
         this.commandManager = commandManager;
     }
     public void runGame() {
         this.gameRunning = true;
-        log.info("\nType player name:");
-        String playerName = InputController.readString();
-        this.player = PlayerBO.builder().name(playerName).build();
-
-        log.info("\nHello " + playerName + "!\n");
-        log.info("Type HELP for a list of available command");
 
         while (gameRunning) {
-            log.info("Where are you going to go?>");
+            log.info("\nWhere are you going to go?>");
 
             commandManager.executeCommand(InputController.readString());
         }
