1c6b2fd963fe8e849bfaff1a3348eb4addc6f37a
enzo.liguori <enzo.liguori.93@hotmail.it>
2023-05-04 14:55:32 +0200

    feature(GameSession): adds the GameSession entity, to persist different gaming session

---
+++ app/src/main/java/pawtropolis/persistence/entity/GameSession.java
@@ -0,0 +1,38 @@
+package pawtropolis.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
+
+@Entity
+@ToString
+@EqualsAndHashCode
+@Getter
+@Table(name = "game_session")
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@Builder
+public class GameSession implements  EntityDB{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "session_name")
+    String sessionName;
+    @OneToOne(cascade =CascadeType.MERGE, fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_player", referencedColumnName = "id")
+    private Player player;
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_room", referencedColumnName = "id")
+    private Room currentRoom;
+    @Column(name = "last_save", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    @UpdateTimestamp
+    private Timestamp timestamp;
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_bag", referencedColumnName = "id")
+    private Bag bag;
+    @Column(name = "life_points")
+    private int lifePoints;
+}
