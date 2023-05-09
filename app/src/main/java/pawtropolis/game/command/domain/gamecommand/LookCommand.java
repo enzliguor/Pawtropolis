82a209792fd9c1ada3c823c9b47645a3089ae62f
enzo.liguori <enzo.liguori.93@hotmail.it>
2023-05-09 16:39:04 +0200

    refactor(LookCommand): optimizes the execute method and adds the adjacentRooms to the description

--- app/src/main/java/pawtropolis/game/command/domain/gamecommand/LookCommand.java
+++ app/src/main/java/pawtropolis/game/command/domain/gamecommand/LookCommand.java
@@ -3,11 +3,15 @@ package pawtropolis.game.command.domain.gamecommand;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import pawtropolis.game.domain.GameSessionBO;
+import pawtropolis.game.domain.LockedRoomBO;
 import pawtropolis.game.domain.RoomBO;
 import pawtropolis.game.domain.animals.domain.AnimalBO;
+import pawtropolis.game.map.util.CardinalPoint;
 
 import java.util.List;
 import java.util.Map;
+import java.util.StringJoiner;
+
 @Slf4j
 @Component
 public class LookCommand extends GameCommand {
@@ -21,30 +25,31 @@ public class LookCommand extends GameCommand {
         RoomBO currentRoom = gameSessionBO.getCurrentRoom();
         Map<String, Integer> itemsName = currentRoom.getItemsName();
         Map<Class<? extends AnimalBO>, List<String>> animals = currentRoom.getAnimalsName();
+        Map<CardinalPoint, RoomBO> adjacentRooms = currentRoom.getAdjacentRooms();
 
-        StringBuilder builder = new StringBuilder("\nYou are in " + currentRoom.getName() + ".\nItems: ");
-
+        StringBuilder builder = new StringBuilder("\n\nYou are in ").append(currentRoom.getName()).append(".");
+        StringJoiner itemJoiner = new StringJoiner(", ");
         for (Map.Entry<String, Integer> entry : itemsName.entrySet()) {
-            builder.append(entry.getKey())
-                    .append("(x").append(entry.getValue())
-                    .append("), ");
+            itemJoiner.add(entry.getKey() + "(x" + entry.getValue() + ")");
         }
-        if (builder.toString().endsWith(", ")) {
-            builder.delete(builder.length()-2, builder.length()-1);
-        }
+        builder.append("\nItems: ").append(itemJoiner.toString());
 
-        builder.append("\nNPC: ");
-
+        StringJoiner animalJoiner = new StringJoiner(", ");
         for (Map.Entry<Class<? extends AnimalBO>, List<String>> entry : animals.entrySet()) {
+            String animalType = entry.getKey().getSimpleName().replaceAll("BO$","");
             for (String animalName : entry.getValue()) {
-                builder.append(animalName)
-                        .append("(").append(entry.getKey().getSimpleName().replaceAll("BO$","")).append(")");
-                builder.append(", ");
+                animalJoiner.add(animalName + "(" + animalType + ")");
             }
         }
-        if (builder.toString().endsWith(", ")) {
-            builder.delete(builder.length()-2, builder.length()-1);
+        builder.append("\nNPC: ").append(animalJoiner.toString());
+
+        StringJoiner roomJoiner = new StringJoiner(", \n");
+        for (Map.Entry<CardinalPoint, RoomBO> entry : adjacentRooms.entrySet()){
+            roomJoiner.add("- " + entry.getKey() + ((entry.getValue() instanceof LockedRoomBO)? " 'LOCKED'" : ""));
         }
-        log.info(builder + "\n");
+        builder.append("\nAdjacent rooms:\n").append(roomJoiner.toString()).append("\n");
+
+        log.info(builder.toString());
+
     }
 }
