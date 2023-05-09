9f3c84b3c9b39f4c71b32f075c4375a3134bd755
enzo.liguori <enzo.liguori.93@hotmail.it>
2023-05-09 16:55:58 +0200

    refactor(SaveCommand): changes the way Room saving is handled.

--- app/src/main/java/pawtropolis/game/command/domain/gamecommand/SaveCommand.java
+++ app/src/main/java/pawtropolis/game/command/domain/gamecommand/SaveCommand.java
@@ -4,20 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import pawtropolis.game.domain.GameSessionBO;
 import pawtropolis.game.domain.PlayerBO;
+import pawtropolis.game.domain.RoomBO;
 import pawtropolis.persistence.service.GameSessionService;
 import pawtropolis.persistence.service.PlayerService;
+import pawtropolis.persistence.service.RoomService;
 
 @Component
 public class SaveCommand extends GameCommand {
 
     private final GameSessionService gameSessionService;
     private final PlayerService playerService;
+    private final RoomService roomService;
 
     @Autowired
-    protected SaveCommand(GameSessionBO gameSessionBO, GameSessionService gameSessionService, PlayerService playerService) {
+    protected SaveCommand(GameSessionBO gameSessionBO, GameSessionService gameSessionService, PlayerService playerService, RoomService roomService) {
         super(gameSessionBO);
         this.gameSessionService = gameSessionService;
         this.playerService = playerService;
+        this.roomService = roomService;
     }
 
 
@@ -25,8 +29,11 @@ public class SaveCommand extends GameCommand {
     public void execute() {
         if(this.gameSessionBO.isNotPersisted()){
             PlayerBO playerBO = this.playerService.saveOrUpdate(this.gameSessionBO.getPlayer());
+            RoomBO roomBO = this.roomService.saveOrUpdate(this.gameSessionBO.getCurrentRoom());
+
             playerBO.setBag(this.gameSessionBO.getPlayer().getBag());
             this.gameSessionBO.setPlayer(playerBO);
+            this.gameSessionBO.setCurrentRoom(roomBO);
         }
         GameSessionBO savedGameSession = this.gameSessionService.saveOrUpdate(this.gameSessionBO);
 
