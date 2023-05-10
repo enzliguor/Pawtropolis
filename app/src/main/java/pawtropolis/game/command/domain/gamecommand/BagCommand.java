21d4100cd648d7a95c64167343ca109a02075797
enzo.liguori <enzo.liguori.93@hotmail.it>
2023-05-10 16:37:17 +0200

    refactor((Bag/Look)Command): modifies these classes so that they will no longer be responsible for constructing the description of Bag and Room objects, but will ask the Descriptor for it

--- app/src/main/java/pawtropolis/game/command/domain/gamecommand/BagCommand.java
+++ app/src/main/java/pawtropolis/game/command/domain/gamecommand/BagCommand.java
@@ -4,8 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import pawtropolis.game.domain.GameSessionBO;
 import pawtropolis.game.domain.PlayerBO;
-
-import java.util.Map;
+import pawtropolis.game.util.Descriptor;
 
 @Component
 @Slf4j
@@ -17,23 +16,8 @@ public class BagCommand extends GameCommand {
 
     @Override
     public void execute() {
-        PlayerBO player = this.gameSessionBO.getPlayer();
-        Map<String, Integer> items = player.getBagContent();
-
-        StringBuilder builder = new StringBuilder("\nIn bag: ");
-
-        for (Map.Entry<String, Integer> entry : items.entrySet()) {
-            builder.append(entry.getKey())
-                    .append("(x").append(entry.getValue())
-                    .append("), ");
-        }
-        if (builder.toString().endsWith(", ")) {
-            builder.delete(builder.length()-2, builder.length()-1);
-        }
-
-        builder.append("   [Available Slot: ")
-                .append(player.getAvailableSlot())
-                .append("]");
-        log.info(builder + "\n");
+        PlayerBO playerBO = this.gameSessionBO.getPlayer();
+        String bagDescription = Descriptor.getBagDescription(playerBO);
+        log.info(bagDescription);
     }
 }
--- app/src/main/java/pawtropolis/game/command/domain/gamecommand/LookCommand.java
+++ app/src/main/java/pawtropolis/game/command/domain/gamecommand/LookCommand.java
@@ -3,14 +3,8 @@ package pawtropolis.game.command.domain.gamecommand;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import pawtropolis.game.domain.GameSessionBO;
-import pawtropolis.game.domain.LockedRoomBO;
 import pawtropolis.game.domain.RoomBO;
-import pawtropolis.game.domain.animals.domain.AnimalBO;
-import pawtropolis.game.map.util.CardinalPoint;
-
-import java.util.List;
-import java.util.Map;
-import java.util.StringJoiner;
+import pawtropolis.game.util.Descriptor;
 
 @Slf4j
 @Component
@@ -22,34 +16,10 @@ public class LookCommand extends GameCommand {
 
     @Override
     public void execute() {
-        RoomBO currentRoom = gameSessionBO.getCurrentRoom();
-        Map<String, Integer> itemsName = currentRoom.getItemsName();
-        Map<Class<? extends AnimalBO>, List<String>> animals = currentRoom.getAnimalsName();
-        Map<CardinalPoint, RoomBO> adjacentRooms = currentRoom.getAdjacentRooms();
+        RoomBO currentRoom = this.gameSessionBO.getCurrentRoom();
+        String roomDescription = Descriptor.getRoomDescription(currentRoom);
 
-        StringBuilder builder = new StringBuilder("\n\nYou are in ").append(currentRoom.getName()).append(".");
-        StringJoiner itemJoiner = new StringJoiner(", ");
-        for (Map.Entry<String, Integer> entry : itemsName.entrySet()) {
-            itemJoiner.add(entry.getKey() + "(x" + entry.getValue() + ")");
-        }
-        builder.append("\nItems: ").append(itemJoiner.toString());
-
-        StringJoiner animalJoiner = new StringJoiner(", ");
-        for (Map.Entry<Class<? extends AnimalBO>, List<String>> entry : animals.entrySet()) {
-            String animalType = entry.getKey().getSimpleName().replaceAll("BO$","");
-            for (String animalName : entry.getValue()) {
-                animalJoiner.add(animalName + "(" + animalType + ")");
-            }
-        }
-        builder.append("\nNPC: ").append(animalJoiner.toString());
-
-        StringJoiner roomJoiner = new StringJoiner(", \n");
-        for (Map.Entry<CardinalPoint, RoomBO> entry : adjacentRooms.entrySet()){
-            roomJoiner.add("- " + entry.getKey() + ((entry.getValue() instanceof LockedRoomBO)? " 'LOCKED'" : ""));
-        }
-        builder.append("\nAdjacent rooms:\n").append(roomJoiner.toString()).append("\n");
-
-        log.info(builder.toString());
+        log.info(roomDescription);
 
     }
 }
