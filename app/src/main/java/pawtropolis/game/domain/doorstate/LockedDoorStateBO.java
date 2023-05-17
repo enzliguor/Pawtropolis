10e64c0a874c2e629e6d3b57979ff0ddc8cea586
enzo.liguori <enzo.liguori.93@hotmail.it>
2023-05-17 12:21:53 +0200

    refactor(LockedDoorStateBO): deletes the DoorUnlocker class and transfers the responsibility of handling door unlocking to the LockedStateDoorBo itself

--- app/src/main/java/pawtropolis/game/domain/doorstate/LockedDoorStateBO.java
+++ app/src/main/java/pawtropolis/game/domain/doorstate/LockedDoorStateBO.java
@@ -2,12 +2,21 @@ package pawtropolis.game.domain.doorstate;
 
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
+import lombok.extern.slf4j.Slf4j;
+import pawtropolis.console.InputController;
 import pawtropolis.game.domain.DoorBO;
 import pawtropolis.game.domain.ItemBO;
+import pawtropolis.game.domain.PlayerBO;
 import pawtropolis.game.domain.RoomBO;
+import pawtropolis.game.util.GameUtility;
 
+import java.util.List;
+import java.util.Set;
+
+
+@Slf4j
 @SuperBuilder
-public class LockedDoorStateBO extends DoorStateBO {
+public class LockedDoorStateBO extends DoorStateBO{
     @Getter
     private final ItemBO key;
 
@@ -16,12 +25,25 @@ public class LockedDoorStateBO extends DoorStateBO {
         this.key = key;
     }
     @Override
-    public RoomBO open(RoomBO currentRoom) {
+    public RoomBO open() {
+        if(askToUnlock()){
+            PlayerBO playerBO = GameUtility.getCurrentPlayer();
+            ItemBO itemKey = chooseItem(playerBO);
+            if (itemKey == null) {
+                return null;
+            }
+            if (!tryToUnlock(itemKey)) {
+                log.info("\nThis is not the right item");
+                return null;
+            }
+            log.info("\nYou unlocked the door!");
+            playerBO.dropItemByName(itemKey.getName());
+            return this.doorBO.open();
+        }
         return null;
     }
 
-    @Override
-    public boolean tryToSwitchState(ItemBO itemKey) {
+    public boolean tryToUnlock(ItemBO itemKey) {
         if(this.key.equals(itemKey)){
             this.doorBO.setState(UnlockedDoorStateBO.builder()
                     .id(1L)
@@ -29,5 +51,21 @@ public class LockedDoorStateBO extends DoorStateBO {
                     .build());
         }
         return !this.doorBO.isLocked();
+    }
+
+    private boolean askToUnlock() {
+        String message = "The door is locked: would you like to use an item to unlock it?";
+        String input = InputController.readChoice(message, List.of("Y", "N"));
+        return input.equals("Y");
+    }
+
+    private ItemBO chooseItem(PlayerBO playerBO) {
+        Set<String> itemsName = playerBO.getBagContent().keySet();
+        if (itemsName.isEmpty()) {
+            log.info("\nYou don't have any item!\nGo pick up some items!");
+            return null;
+        }
+        String input = InputController.readChoice("Type the name of the chosen item", itemsName);
+        return playerBO.getItemByName(input);
     }
 }
