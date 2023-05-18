a99984c4bc46463c9a3f43963bf63a69d0e242c5
Vincenzo Liguori <enzo.liguori.93@hotmail.it>
2023-05-18 12:40:48 +0200

    refactor(SaveCommand): transfers to the GameSessionService the responsibility of handling the 'save' of the gameSessionBO

--- app/src/main/java/pawtropolis/persistence/service/GameSessionService.java
+++ app/src/main/java/pawtropolis/persistence/service/GameSessionService.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import pawtropolis.game.domain.GameSessionBO;
+import pawtropolis.game.domain.PlayerBO;
+import pawtropolis.game.domain.RoomBO;
 import pawtropolis.persistence.dao.GameSessionDAO;
 import pawtropolis.persistence.entity.GameSession;
 import pawtropolis.persistence.marshaller.Marshaller;
@@ -15,9 +17,27 @@ import java.util.List;
 public class GameSessionService extends AbstractService<GameSession, Long, GameSessionBO> {
 
 
+    private final PlayerService playerService;
+    private final RoomService roomService;
+
     @Autowired
-    public GameSessionService(JpaRepository<GameSession, Long> dao, Marshaller<GameSession, GameSessionBO> marshaller) {
+    public GameSessionService(JpaRepository<GameSession, Long> dao, Marshaller<GameSession, GameSessionBO> marshaller, PlayerService playerService, RoomService roomService) {
         super(dao, marshaller);
+        this.playerService = playerService;
+        this.roomService = roomService;
+    }
+
+    @Override
+    public GameSessionBO saveOrUpdate(GameSessionBO gameSessionBO){
+        if(gameSessionBO.isNotPersisted()){
+            PlayerBO playerBO = this.playerService.saveOrUpdate(gameSessionBO.getPlayer());
+            RoomBO roomBO = this.roomService.saveOrUpdate(gameSessionBO.getCurrentRoom());
+
+            playerBO.setBag(gameSessionBO.getPlayer().getBag());
+            gameSessionBO.setPlayer(playerBO);
+            gameSessionBO.setCurrentRoom(roomBO);
+        }
+        return super.saveOrUpdate(gameSessionBO);
     }
 
     public List<String> findSessionNamesByPlayerId(Long id){
