0b3d48403473e5c17c07b7c3631621fe3456272a
Vincenzo Liguori <enzo.liguori.93@hotmail.it>
2023-05-19 18:15:24 +0200

    refactor(GameSessionDAO): change the return type of the method 'findSessionNamesByPlayerId'

--- app/src/main/java/pawtropolis/persistence/dao/GameSessionDAO.java
+++ app/src/main/java/pawtropolis/persistence/dao/GameSessionDAO.java
@@ -6,13 +6,13 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pawtropolis.persistence.entity.GameSession;
 
-import java.util.List;
+import java.util.Set;
 
 @Repository
 public interface GameSessionDAO extends JpaRepository<GameSession, Long> {
 
     @Query("SELECT gs.sessionName FROM GameSession gs WHERE gs.player.id = :playerId")
-    List<String> findSessionNamesByPlayerId(@Param("playerId") Long playerId);
+    Set<String> findSessionNamesByPlayerId(@Param("playerId") Long playerId);
 
     GameSession findByPlayer_IdAndSessionName(Long playerId, String sessionName);
 
