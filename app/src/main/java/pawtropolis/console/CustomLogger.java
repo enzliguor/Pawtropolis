39cae15accb296e2954f8d30359980cdae6436a5
enzo.liguori <enzo.liguori.93@hotmail.it>
2023-05-20 20:08:31 +0200

    fix(CustomLogger): fixes the colors RED and YELLOW that were inverted

--- app/src/main/java/pawtropolis/console/CustomLogger.java
+++ app/src/main/java/pawtropolis/console/CustomLogger.java
@@ -8,8 +8,8 @@ import lombok.extern.slf4j.Slf4j;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CustomLogger {
 
-    private static final String RED = "\u001B[33m";
-    private static final String YELLOW = "\u001B[31m";
+    private static final String RED = "\u001B[31m";
+    private static final String YELLOW = "\u001B[33m";
     private static final String BLUE = "\u001B[36m";
     private static final String BLANK = "\u001B[0m";
 
