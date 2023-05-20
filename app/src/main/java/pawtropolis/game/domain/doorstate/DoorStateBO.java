eed7f90f857c21e2e6563ad56b1872578512af18
enzo.liguori <enzo.liguori.93@hotmail.it>
2023-05-20 18:46:19 +0200

    refactor(DoorStateBO): adds the method 'switchState' and remove the field id, because only the LockedDoorStateBO will have own identity

--- app/src/main/java/pawtropolis/game/domain/doorstate/DoorStateBO.java
+++ app/src/main/java/pawtropolis/game/domain/doorstate/DoorStateBO.java
@@ -5,20 +5,21 @@ import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 import pawtropolis.game.domain.BusinessObject;
 import pawtropolis.game.domain.DoorBO;
+import pawtropolis.game.domain.ItemBO;
 import pawtropolis.game.domain.RoomBO;
 
 @SuperBuilder
 @Getter
 @Setter
 public abstract class DoorStateBO implements BusinessObject {
-
-    protected Long id;
     protected DoorBO doorBO;
 
     protected DoorStateBO(DoorBO doorBO) {
         this.doorBO = doorBO;
     }
 
-    public abstract RoomBO open();
+    public abstract RoomBO open(RoomBO currentRoom);
+
+    public abstract boolean switchState(ItemBO itemKey);
 
 }
